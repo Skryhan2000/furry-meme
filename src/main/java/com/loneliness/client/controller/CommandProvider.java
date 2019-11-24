7e232e1604511ad470089f8599d7d72fc1bf86ad
EgorKrs <47493003+egorkrs@users.noreply.github.com>
2019-11-24 15:39:53 +0300

    add validation

--- src/main/java/com/loneliness/client/controller/CommandProvider.java
+++ src/main/java/com/loneliness/client/controller/CommandProvider.java
@@ -2,6 +2,9 @@ package com.loneliness.client.controller;
 
 import com.loneliness.client.controller.command_impl.differential_indicators.*;
 import com.loneliness.client.controller.command_impl.user.*;
+import com.loneliness.client.controller.command_impl.validation_command.DifferentialIndicatorsValidation;
+import com.loneliness.client.controller.command_impl.validation_command.IndexValidation;
+import com.loneliness.client.controller.command_impl.validation_command.UserDataValidation;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,6 +20,7 @@ public class CommandProvider {
         repository.put(CommandName.UPDATE_USER,new UpdateUser());
         repository.put(CommandName.RECEIVE_ALL_USERS_IN_LIMIT,new ReceiveAllUsersInLimit());
         repository.put(CommandName.RECEIVE_USER,new ReceiveUser());
+        repository.put(CommandName.USER_DATA_VALIDATION,new UserDataValidation());
 
         repository.put(CommandName.CREATE_DIFFERENTIAL_INDICATORS, new CreateDifferentialIndicators());
         repository.put(CommandName.DELETE_DIFFERENTIAL_INDICATORS,new DeleteDifferentialIndicators());
@@ -24,6 +28,9 @@ public class CommandProvider {
         repository.put(CommandName.RECEIVE_ALL_DIFFERENTIAL_INDICATORS_IN_LIMIT,new ReceiveAllDifferentialIndicatorsInLimit());
         repository.put(CommandName.RECEIVE_DIFFERENTIAL_INDICATORS,new ReceiveDifferentialIndicators());
         repository.put(CommandName.UPDATE_DIFFERENTIAL_INDICATORS,new UpdateDifferentialIndicators());
+        repository.put(CommandName.DIFFERENTIAL_INDICATORS_VALIDATION,new DifferentialIndicatorsValidation());
+
+        repository.put(CommandName.INDEX_VALIDATION,new IndexValidation());
 
     }
     public Map<CommandName, Command> getRepository() {
---
+++ src/main/java/com/loneliness/client/controller/command_impl/validation_command/UserDataValidation.java
@@ -0,0 +1,16 @@
+package com.loneliness.client.controller.command_impl.validation_command;
+
+import com.loneliness.client.controller.Command;
+import com.loneliness.client.controller.ControllerException;
+import com.loneliness.client.service.DataValidationFactory;
+import com.loneliness.entity.UserData;
+
+import javax.validation.ConstraintViolation;
+import java.util.Set;
+
+public class UserDataValidation implements Command<UserData, Set<ConstraintViolation<UserData>>> {
+    @Override
+    public Set<ConstraintViolation<UserData>>  execute(UserData request) throws ControllerException {
+        return DataValidationFactory.getValidatorFactory().validate(request);
+    }
+}
